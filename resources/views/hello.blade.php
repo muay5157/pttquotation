cd19b74a4be7a3de79fde771098661cac29602c0
Panut Chongpakdee <panutchongpakdee@hotmail.com>
2017-10-29 11:37:38 +0700

    test route

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello world blade
                 </div>
 
                 <div class="links">
